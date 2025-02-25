088540b3a30912abd36d337e226ee9f57f567eb8
Pedja02 <predrag.tanaskovic1317@gmail.com>
2025-02-25 14:49:43 +0100

    cvetak poruka

--- src/main/java/rs/malkier/github_actions_test/Kontroler.java
+++ src/main/java/rs/malkier/github_actions_test/Kontroler.java
@@ -3,18 +3,19 @@ package rs.malkier.github_actions_test;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/zdravo")
-public class Kontroler {
+            @RequestMapping("/zdravo")
+public      class Kontroler {
 
-  @GetMapping
+            @GetMapping
   public String hello() {
-    return "Zato sto sam tiiii STAAAAA";
+    return "Ja sam cvetak";
   }
 
-  @GetMapping("/vatrogasac")
+            @GetMapping("/vatrogasac")
   public String hello2() {
-    return "VEROVALAAA SLEEEPOOOOOOO";
+    return "cudan cvetak";
   }
 }
