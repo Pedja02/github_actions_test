b7f62149ffdbb96699898074f64524127784b490
Pedja02 <predrag.tanaskovic1317@gmail.com>
2025-02-25 14:33:52 +0100

    added second mapping

---
+++ src/main/java/rs/malkier/github_actions_test/Kontroler.java
@@ -0,0 +1,20 @@
+package rs.malkier.github_actions_test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+      @RestController
+@RequestMapping("/zdravo")
+public    class Kontroler {
+
+        @GetMapping
+      public String hello() {
+    return "Zato sto sam tiiii STAAAAA";
+  }
+
+  @GetMapping("/vatrogasac")
+        public String hello2() {
+          return "Milivoje vatrogasav";
+  }
+}
--- src/test/java/rs/malkier/github_actions_test/GithubActionsTestApplicationTests.java
+++ src/test/java/rs/malkier/github_actions_test/GithubActionsTestApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GithubActionsTestApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
+  @Test
+  void contextLoads() {}
 }
